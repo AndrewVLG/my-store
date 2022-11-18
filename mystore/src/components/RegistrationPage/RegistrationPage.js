b47cba009fc2e0de4c2ac99efb9cac6302e4775c
AndrewVLG <114805157+AndrewVLG@users.noreply.github.com>
2022-11-19 01:03:03 +0300

    add registration Page

--- mystore/src/components/RegistrationPage/RegistrationPage.js
+++ mystore/src/components/RegistrationPage/RegistrationPage.js
@@ -1,9 +1,27 @@
-
+import styles from './RegistrationPage.module.css';
 
 const RegistrationPage = () => {
 
     return (
-        <h1>This is Registration Page!</h1>
+        <div className={styles.wrapper}>
+            <h1>My-Store</h1>
+            <h2>Registration Page</h2>
+            <form>
+                <label>E-mail: </label>
+                <input placeholder='Enter your E-mail'/>
+                <label>Password </label>
+                <input placeholder='Enter Password' type='password'/>
+                <label>Confirm password </label>
+                <input placeholder='Confirm Password' type='password'/>
+                <label>Nickname: </label>
+                <input placeholder='Enter Nickname'/>
+                <label>First name: </label>
+                <input placeholder='Enter your First name'/>
+                <label>Last name: </label>
+                <input placeholder='Enter your Last name'/>
+                <button>Register</button>
+            </form>
+        </div>
     )
 }
 
